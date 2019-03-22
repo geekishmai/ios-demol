01f3e1d79ab7b8f30dd0312ebaec110268b89de4
user <user@userdemac-mini.local>
2019-03-22 10:17:24 +0800

    Initial Commit

---
+++ 登录界面demol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  登录界面demol
+//
+//  Created by user on 2019/3/22.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 登录界面demol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  登录界面demol
+//
+//  Created by user on 2019/3/22.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
